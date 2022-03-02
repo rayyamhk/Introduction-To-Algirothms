8261316282a6eae77a27c0981d4dd72a6e0e357a
Ray Yam <ryamhkdev@gmail.com>
2022-03-03 03:52:22 +0800

    chapter 6

---
+++ chapters/chapter6/1_Heap-sort.js
@@ -0,0 +1,83 @@
+// Chapter 6.1 to 6.4 Heap
+
+/**
+ * The heap data structure is a complete binary tree,
+ * each of the node (except the root node) satisfies the heap properties.
+ * Max-heap property: the value of a node is at most the value of its parent.
+ */
+
+module.exports = {heapSort, maxHeapify};
+
+/**
+ * 1. Build a max-heap from the input array. O(N)
+ * 2. Swap the first element with the last element of the heap, then decrement the size of the heap. O(1)
+ * 3. max-heapify the root of the heap. O(logM), where M is the heap size.
+ * 4. Repeat 2 and 3 until the array is sorted. N - 1
+ * O(logN) + O(log(N-1)) + ... + O(log(1)) = O(log(N!)) = O(NlogN)
+ * Worst == Best == O(NlogN)
+ * O(N) if all keys are equal.
+ * Space: O(1)
+ * 
+ * Note: In general, merge sort performs better than heap sort because of the smaller constant factor
+ * Also, heap sort is unstable, i.e. the order of the same keys are not guaranteed.
+ */
+function heapSort(A) {
+  buildMaxHeap(A);
+  for (let i = A.length - 1; i > 0; i--) {
+    swap(A, 0, i);
+    A.heapSize -= 1;
+    maxHeapify(A, 0);
+  }
+}
+
+/**
+ * Assume the subtree rooted at left(i) and right(i) are max-heaps,
+ * but A[i] may not satisfy the max-heap property.
+ * It swaps the A[i] downward until the subtree rooted at i satisfies the property.
+ * Best: O(1), Worst: O(N), where N is the size of the subtree.
+ */
+function maxHeapify(A, i) {
+  const l = left(i);
+  const r = right(i);
+  let largest = i
+  if (l < A.heapSize && A[l] > A[i]) {
+    largest =  l;
+  }
+  if (r < A.heapSize && A[r] > A[largest]) {
+    largest = r;
+  }
+  if (largest === i) {
+    return;
+  }
+  swap(A, i, largest);
+  return maxHeapify(A, largest);
+}
+
+/**
+ * Build the max-heap in bottom-up approach.
+ * Worst == Best == O(N);
+ */
+function buildMaxHeap(A) {
+  A.heapSize = A.length;
+  for (let i = Math.floor(A.length / 2) - 1; i >= 0; i--) {
+    maxHeapify(A, i);
+  }
+}
+
+function parent(i) {
+  return Math.floor((i - 1) / 2);
+}
+
+function left(i) {
+  return 2 * i;
+}
+
+function right(i) {
+  return 2 * i + 1;
+}
+
+function swap(A, i, j) {
+  const temp = A[i];
+  A[i] = A[j];
+  A[j] = temp;
+}
--- index.js
+++ index.js
@@ -3,15 +3,43 @@ const selectionSort = require('./chapters/chapter2/2_Selection-sort');
 const mergeSort = require('./chapters/chapter2/3_Merge-sort');
 const binarySearch = require('./chapters/chapter2/4_Binary-search');
 const bubbleSort = require('./chapters/chapter2/5_Bubble-sort');
+const {heapSort} = require('./chapters/chapter6/1_Heap-sort');
+const PriorityQueue = require('./chapters/chapter6/2_Priority-queue');
 const {maximumSubarray, maximumSubarrayIterative, maximumSubarrayIterativeLinear} = require('./chapters/chapter4/1_Maximum-subarray');
 const {matrixMultiplication, StrassenAlgorithm} = require('./chapters/chapter4/2_Matrix-multiplication');
 
-const items = getRandomArray(10000000, -100, 100);
+// const items1 = getRandomArray(10000, -100, 100);
+// const items2 = [...items1];
+// const items3 = [...items1];
 
-const A = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8], [7, 8, 9, 10]];
-const B = [[5, 6, 7, 8], [7, 8, 9, 10], [9, 10, 11, 12], [11, 12, 13, 14]];
-console.log(matrixMultiplication(A, B));
-console.log(StrassenAlgorithm(A, B));
+// const A = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8], [7, 8, 9, 10]];
+// const B = [[5, 6, 7, 8], [7, 8, 9, 10], [9, 10, 11, 12], [11, 12, 13, 14]];
+// console.log(matrixMultiplication(A, B));
+// console.log(StrassenAlgorithm(A, B));
+
+// console.time('heap-sort');
+// heapSort(items1);
+// console.timeEnd('heap-sort');
+
+// console.time('merge-sort');
+// mergeSort(items2);
+// console.timeEnd('merge-sort');
+
+const priorityQueue = new PriorityQueue();
+priorityQueue.insert(10);
+priorityQueue.insert(20);
+priorityQueue.insert(30);
+console.log(priorityQueue.popMax());
+console.log(priorityQueue.heap);
+console.log(priorityQueue.popMax());
+console.log(priorityQueue.heap);
+console.log(priorityQueue.popMax());
+console.log(priorityQueue.heap);
+priorityQueue.insert(100);
+console.log(priorityQueue.heap);
+// console.log(priorityQueue.max())
+// console.log(priorityQueue.popMax());
+// console.log(priorityQueue.heap, priorityQueue.heap.size);
 
 function getRandomArray(size, min, max) {
   const arr = new Array(size);
